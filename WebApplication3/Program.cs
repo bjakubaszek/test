3ffff3d3c8384c9f1b82bd849475a9156dba8500
bjakubaszek <s27392@pjwstk.edu.pl>
2025-06-07 21:10:05 +0200

    1st

---
+++ WebApplication3/Program.cs
@@ -0,0 +1,35 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDbService, DbService>();
+
+builder.Services.AddDbContext<MyDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
